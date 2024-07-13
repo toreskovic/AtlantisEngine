e8ee2f59f84ce786029e7ea37cbb50ce653aba36
Tomislav <tomislav.oreskovic.125@gmail.com>
2024-07-13 14:24:35 +0200

    timesliced systems now handle delta time properly

--- src/engine/system.cpp
+++ src/engine/system.cpp
@@ -6,20 +6,35 @@ namespace Atlantis
 {
   void ASystem::Process(AWorld *world)
   {
+    CompoundedDeltaTime = world->GetDeltaTime();
+  }
+
+  float ASystem::GetDeltaTime() const
+  {
+    return CompoundedDeltaTime;
   }
 
   void ALambdaSystem::Process(AWorld *world)
   {
+    CompoundedDeltaTime = world->GetDeltaTime();
     Lambda(world);
   }
 
   void ALambdaSystemTimesliced::Process(AWorld* world)
   {
+    if (CurrentObjectIndex == 0)
+    {
+      CompoundedDeltaTime = 0.0f;
+    }
+
+    CompoundedDeltaTime += world->GetDeltaTime();
+
     LambdaTimesliced(world, this);
   }
 
   void ASyncSystem::Process(AWorld *world)
   {
+    CompoundedDeltaTime = world->GetDeltaTime();
     world->SyncEntities();
   }
 }
