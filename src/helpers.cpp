9c44f0b9add4202d01c86c471122baa9b05665e8
Tomislav Oreskovic <tomislav.oreskovic.125@gmail.com>
2024-10-19 15:16:48 +0200

    - added box2d 3.0 - set opengl to version 4.3 - enabled avx2 - minor changes to support clang on windows (but probably broke MSVC)

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -1,20 +1,22 @@
 #include "helpers.h"
 
-#include <iostream>
-#include <fstream>
 #include <filesystem>
+#include <fstream>
+#include <iostream>
 
 #if defined(_WIN32)
-#if defined(__amd64__) || defined(__amd64) || defined(__x86_64__) || defined(__x86_64) || defined(_M_X64) || defined(_M_AMD64)
+#if defined(__amd64__) || defined(__amd64) || defined(__x86_64__) ||           \
+    defined(__x86_64) || defined(_M_X64) || defined(_M_AMD64)
 #define _AMD64_
-#elif defined(i386) || defined(__i386) || defined(__i386__) || defined(__i386__) || defined(_M_IX86)
+#elif defined(i386) || defined(__i386) || defined(__i386__) ||                 \
+    defined(__i386__) || defined(_M_IX86)
 #define _X86_
 #elif defined(__arm__) || defined(_M_ARM) || defined(_M_ARMT)
 #define _ARM_
 #endif
 
-#include <minwindef.h>
 #include <libloaderapi.h>
+#include <minwindef.h>
 #elif defined(__linux__)
 #include <limits.h>
 #include <unistd.h>
@@ -22,32 +24,30 @@
 
 namespace Atlantis
 {
-    std::filesystem::path Helpers::GetExeDirectory()
-    {
+std::filesystem::path Helpers::GetExeDirectory()
+{
 #ifdef _WIN32
-        // Windows specific
-        wchar_t szPath[MAX_PATH];
-        GetModuleFileNameW(NULL, szPath, MAX_PATH);
+    // Windows specific
+    wchar_t szPath[MAX_PATH];
+    GetModuleFileNameW(NULL, szPath, MAX_PATH);
 #else
-        // Linux specific
-        char szPath[PATH_MAX];
-        ssize_t count = readlink("/proc/self/exe", szPath, PATH_MAX);
-        if (count < 0 || count >= PATH_MAX)
-            return {}; // some error
-        szPath[count] = '\0';
+    // Linux specific
+    char szPath[PATH_MAX];
+    ssize_t count = readlink("/proc/self/exe", szPath, PATH_MAX);
+    if (count < 0 || count >= PATH_MAX)
+        return {}; // some error
+    szPath[count] = '\0';
 #endif
-        return std::filesystem::path{szPath}.parent_path() / ""; // to finish the folder path with (back)slash
-    }
+    return std::filesystem::path{ szPath }.parent_path() /
+           ""; // to finish the folder path with (back)slash
+}
 
-    std::filesystem::path Helpers::GetProjectDirectory()
-    {
-        std::string projectName;
-        std::ifstream projectFile("./project.aeng");
-        std::getline(projectFile, projectName);
-#ifdef _WIN32
-        return std::filesystem::path(GetExeDirectory().string() + "../../projects/" + projectName + "/");
-#else
-        return std::filesystem::path("../projects/" + projectName + "/");
-#endif
-    }
+std::filesystem::path Helpers::GetProjectDirectory()
+{
+    std::string projectName;
+    std::ifstream projectFile("./project.aeng");
+    std::getline(projectFile, projectName);
+
+    return std::filesystem::path("../projects/" + projectName + "/");
+}
 }
